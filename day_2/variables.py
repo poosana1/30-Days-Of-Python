16e44854c9084cd014670750c8305711d24c5b7d
poosana1 <poosana.dev@gmail.com>
2026-06-29 23:05:18 +0700

    completed day 2 challenge

---
+++ day_2/variables.py
@@ -0,0 +1,44 @@
+# Day 2: 30 Days of python programming
+
+first_name = 'Poosana'
+last_name = 'Thassanavisut'
+full_name = first_name + ' ' + last_name 
+country = 'Thailand'
+city = 'Chiang Mai'
+age = 25
+year = 2026
+is_married = False
+is_true = True
+is_light_on = True
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+print(len(first_name))
+compare = len(first_name) > len(last_name)
+num_one = 5, num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+area_of_circle = 3.14 * 30 ** 2
+circum_of_circle = 2 * 3.14 * 30
+
+radius = int(input("Enter radius: "))
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+country = input("Enter your country: ")
+age = int(input("Enter your age: "))
