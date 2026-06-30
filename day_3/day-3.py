406244534e2c0e875e6f019fbd5be09110cbe394
poosana1 <poosana.dev@gmail.com>
2026-06-30 23:37:52 +0700

    day 03 - hw 17

---
+++ day_3/day-3.py
@@ -0,0 +1,43 @@
+## 1 
+age = 25
+height = 1.62
+complex_num = 4 + 3j
+
+## 4
+base = int(input("Enter base: "))
+height = int(input("Enter height: "))
+area_of_triangle = 0.5 * base * height
+print("The area of the triangle is " + str(area_of_triangle))
+
+## 5 
+a = int(input('Enter side a: '))
+b = int(input('Enter side b: '))
+c = int(input('Enter side c: '))
+print("The perimeter of the triangle is " + str(a+b+c))
+
+## 6 
+length = int(input('Enter rectangle length: '))
+height = int(input('Enter rectangle height: '))
+rectangle_perimeter = (2*length) + (2*height)
+print("The rectangle perimeter is ", rectangle_perimeter)
+
+## 7
+radius = int(input('Enter circle radius: '))
+circle_area = 3.14 * radius ** 2
+circle_circumference = 2 * 3.14 * radius
+
+## 8, 9, 10, 11
+#12
+is_true = len('python') == len('dragon')
+
+#13
+print (('on' in 'python') and ('on' in 'dragon'))
+
+#14
+print('jargon' in 'I hope this course is not full of jargon')
+
+#15
+#16 
+str(float(len('python')))
+
+## 17
