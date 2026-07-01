977d33b39ec3c6165cd1f0e8ca778adb9dfcb033
poosana1 <poosana.dev@gmail.com>
2026-07-01 23:41:23 +0700

    fix name

--- day_2/variables.py
+++ day_2/variables.py
@@ -1,7 +1,7 @@
 # Day 2: 30 Days of python programming
 
-first_name = 'Phu'
-last_name = 'Thas'
+first_name = 'Poosana'
+last_name = 'Thassanavisut'
 full_name = first_name + ' ' + last_name 
 country = 'Thailand'
 city = 'Chiang Mai'
